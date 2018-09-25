cbdc8f5370e1342a7e80f442466e6c6714959e5f
Will Qin <qq554552354@126.com>
2018-09-25 00:00:37 -0700

    Update client.c

--- client.c
+++ client.c
@@ -8,15 +8,13 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#define RET_VAL(VAL,FUNC_NAME) do{\
-                    if(VAL < 0) \
-                    {\
-                        perror(#FUNC_NAME);\
-                        exit(-1);\
-                    }\
-                }\
-                while(0)
+
 int main(int argc, char * argv[]) {
+    if(argc<3){
+        printf("missing parameter\n");
+        return 0;
+    }
+    printf("initialize client...\n");
     int sock_fd;
     int client_fd;
     int ret;
@@ -27,21 +25,16 @@ int main(int argc, char * argv[]) {
     struct in_addr inaddr;
     int addrlen = sizeof(clientaddr);
     char message[200];
-    char *sendstr = "hello client\n";
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(3456);
-    ret = inet_aton("127.0.0.1", &inaddr);
-    RET_VAL(ret, inet_aton);
+    addr.sin_port = htons(atoi(argv[2]));
+    ret = inet_aton(argv[1], &inaddr);
     addr.sin_addr = inaddr;
 
     sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     ret = connect(sock_fd, (struct sockaddr *) &addr, sizeof(addr));
-    RET_VAL(ret, connect);
     bzero(message, sizeof(message));
-//    ret = recv(client_fd,message,sizeof(message),0);
-//    RET_VAL(ret,recv);
-//    printf("%s",message);
+    printf("client ready\n\n\n");
     while (1) {
         timeout.tv_sec = 5;
         timeout.tv_usec = 5000000;
@@ -52,10 +45,9 @@ int main(int argc, char * argv[]) {
         if (ret < 0) {
             perror("select");
         } else if (0 == ret) {
-            printf("time is over\n");
+            //printf("time is over\n");
         } else {
             if (FD_ISSET(0, &readfdset)) {
-                printf("select ok\n");
                 ret = read(0, message, sizeof(message));
                 if (ret < 0) {
                     perror("read");
@@ -66,22 +58,17 @@ int main(int argc, char * argv[]) {
                 }
             }
             if (FD_ISSET(sock_fd, &readfdset)) {
+                bzero(message, sizeof(message));
                 ret = recv(sock_fd, message, sizeof(message), 0);
                 if (0 == ret) {
                     printf("server not in work\n");
                     break;
                 } else {
-                    message[ret] = '\0';
-                    printf("recv:%s\n", message);
+                    message[ret-1] = '\0';
+                    printf(">>>%s\n", message);
                 }
             }
         }
     }
-    ret = send(sock_fd, sendstr, strlen(sendstr), 0);
-    RET_VAL(ret, send);
-    printf("send string success\n");
-
-//    ret = recv(sock_fd,message,sizeof(message),0);
-//    printf("recv from ser:%s",message);
     close(sock_fd);
 }
