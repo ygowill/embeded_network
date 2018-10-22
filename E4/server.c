8931cee966cf4a8f64b52acbde9b4a435d8ca716
ygowill <qq554552354@126.com>
2018-10-22 12:14:54 +0800

    edit E4 server.c

--- E4/server.c
+++ E4/server.c
@@ -42,6 +42,10 @@ int main(int argc, char** argv) {
     listen(listen_socket, 5);
 
     signal(SIGCHLD, sigChildFun);
+    fd_set         readfdset;
+    struct timeval timeout;
+    timeout.tv_sec  = 60;
+    timeout.tv_usec = 60000000;
     printf("server ready.\n\n");
 
     while (1) {
@@ -60,9 +64,8 @@ int main(int argc, char** argv) {
             }
         }
         else {
-            printf("new connection from [%s]\n",inet_ntoa(clientAddr.sin_addr));
+            printf("new connection from [%s]\n", inet_ntoa(clientAddr.sin_addr));
         }
-
 
         int tmpPid = fork();
         if (tmpPid == 0) {
@@ -75,22 +78,47 @@ int main(int argc, char** argv) {
 
             int ret = 0;
             while (1) {
-                bzero(buf, sizeof(buf));
-                ret = ( int )read(conn_socket, buf, sizeof(buf));
-                buf[ret-1]='\0';
-                if (ret < 0) {
-                    perror("read error");
-                    printf("socket %d closed, child process exit\n", conn_socket);
-                    close(conn_socket); // 子进程让通信的socket计数减1
-                    return -2;          // 子进程退出
+                FD_ZERO(&readfdset);
+                FD_SET(conn_socket, &readfdset);
+                FD_SET(0, &readfdset);
+                ret = select(conn_socket + 1, &readfdset, NULL, NULL, &timeout);
+                if (ret < -1) {
+                    perror("select");
                 }
                 else {
-                    if (strncmp("exit", buf, 4) == 0) {
-                        printf("receive exit from client, close child process\n");
-                        break;
+                    if (FD_ISSET(conn_socket, &readfdset)) {
+                        bzero(buf, sizeof(buf));
+                        ret      = ( int )read(conn_socket, buf, sizeof(buf));
+                        buf[ret] = '\0';
+                        if (ret < 0) {
+                            perror("read error");
+                            printf("socket %d closed, child process exit\n", conn_socket);
+                            close(conn_socket); // 子进程让通信的socket计数减1
+                            return -2;          // 子进程退出
+                        }
+                        else {
+                            if (strncmp("exit", buf, 4) == 0) {
+                                printf("receive exit from client, close child process\n");
+                                break;
+                            }
+                            printf("get message \"%s\"\n", buf);
+                            send(conn_socket, buf, sizeof(buf), 0);
+                        }
                     }
-                    printf("get message \"%s\"\n", buf);
-                    send(conn_socket, buf, sizeof(buf), 0);
+                    if (FD_ISSET(0, &readfdset)) {
+                        ret = ( int )read(0, buf, sizeof(buf));
+                        if (ret < 0) {
+                            perror("read");
+                        }
+                        else {
+                            buf[ret] = '\0';
+                        }
+                        ret = strncmp(buf, "exit", 4);
+                        if (ret == 0) {
+                            printf("server exit.\n");
+                            break;
+                        }
+                    }
                 }
             }
 
